d34c84a9adab38846d2393fb3d6adab4d3d2e5b7
Christer Weinigel <christer@weinigel.se>
2016-07-18 20:36:31 +0200

    Document why I have my own openocd and add a fallback

--- host/boot-jtag.sh
+++ host/boot-jtag.sh
@@ -1,10 +1,25 @@
-#! /bin/sh
+#! /bin/bash
 cd "`dirname \"$0\"`"
+
+# Newer versions of openocd are much faster.  It takes 10 seconds to
+# load a 5MByte image with openocd-0.10 instead of 30 seconds with
+# openocd-0.7 which is the version that comes with Linux Mint.
+#
+# Use the openocd-0.10 I have compiled and put in /opt by default and
+# fall back to the system openocd if that one can't be found.
+
+for OPENOCD in /opt/openocd/bin/openocd openocd; do
+    if [ -x "$OPENOCD" ]; then
+	break
+    fi
+done
+
+echo "Using $OPENOCD"
 
 # Kill openocd, it might be hung
 killall -9 openocd 2>/dev/null
 sleep 0.3
-/opt/openocd/bin/openocd -f openocd.cfg &
+$OPENOCD -f openocd.cfg &
 
 # Wait for openocd to initialize and tell it to reset the SoC
 sleep 0.3
@@ -19,7 +34,7 @@ killall -9 openocd
 #
 # Start openocd again, tell it to initialize and halt the Soc
 sleep 0.4
-/opt/openocd/bin/openocd -f openocd.cfg -c init -c halt &
+$OPENOCD -f openocd.cfg -c init -c halt &
 
 # openocd should now be running with the SoC halted
 sleep 0.3
